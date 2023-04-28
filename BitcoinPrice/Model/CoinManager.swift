2b1f4e8c6aca09991df7dbf65d2a8b9a0f963697
Ata <atakanozalan@gmail.com>
2023-04-28 15:20:41 +0300

    Creating UI Parts

---
+++ BitcoinPrice/Model/CoinManager.swift
@@ -0,0 +1,15 @@
+//
+//  CoinManager.swift
+//  BitcoinPrice
+//
+//  Created by Ezagor on 28.04.2023.
+//
+
+import Foundation
+
+struct CoinManager {
+    let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
+    let apiKey = "YOUR_API_KEY_HERE"
+    
+    let currencyArray = ["USD","TL","EUR","GBP"]
+}
